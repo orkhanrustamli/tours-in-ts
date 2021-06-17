e53c1517a877ebad8f7ec1a0526d3648d440e102
Orkhan Rustamli <orkhan.rustamli.96@gmail.com>
2021-06-17 15:23:15 +0300

    deleted build files

--- build/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function catchAsync(func) {
-    return function (req, res, next) {
-        func(req, res, next).catch(next);
-    };
-}
-exports.default = catchAsync;
-"";
